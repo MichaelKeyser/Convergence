c357ee6895c7484937d7bc960711a1e76916cdfd
Ben Huenemann <ben41huenemann@gmail.com>
2020-10-17 15:35:55 -0600

    Created a longest path algorithm.
    This algorithm takes in a sequence of numbers and outputs the longest subsequence that converges to that value

--- Convergence.py
+++ Convergence.py
@@ -1,6 +1,9 @@
 import sys
 
-def longest_sequence(sequence):
+
+def longest_sequence(sequence, limit):
+    max_path = list()
+
     l_bound = sys.maxsize
     u_bound = -sys.maxsize
 
@@ -11,6 +14,29 @@ def longest_sequence(sequence):
             u_bound = sequence[i]
 
     for i in range(0, len(sequence)):
-        longest_path(sequence, i)
+        if i >= len(sequence) - len(max_path):
+            break
 
-def longest_path(sequence, start):
+        epsilon = abs(sequence[i] - limit)
+
+        cur_path = longest_path(sequence, i, limit, epsilon + 1, len(max_path))
+        if len(cur_path) > len(max_path):
+            max_path = cur_path
+    return max_path
+
+
+def longest_path(sequence, start, limit, epsilon, max_len):
+    if abs(sequence[start] - limit) >= epsilon:
+        return list()
+    if start == len(sequence):
+        return list(range(start, start + 1))
+
+    max_path = list()
+    for i in range(start + 1, len(sequence)):
+        cur_path = list(range(start, start + 1)) + longest_path(sequence, i, limit, abs(sequence[start] - limit), max_len)
+        if len(cur_path) > len(max_path):
+            max_path = cur_path
+    return max_path
+
+
+print(longest_sequence([1, 9, 4, 5, 2, 9, 4, 5, 2, 4, 2], 5))
