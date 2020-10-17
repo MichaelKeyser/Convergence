4e042ff12a3f08a7622df103f8473b7b04f78219
Michael Keyser <Michael@Michaels-MacBook-Air-4.local>
2020-10-17 15:40:35 -0600

    completed functions that generate sequences

--- functions.py
+++ functions.py
@@ -8,5 +8,10 @@ def generate_random_sequence(lower, upper, n):
         sequence.append(r.uniform(lower,upper))
     return sequence
 
-def generate_specific_sequence(n):
-    print("finish")
+def generate_specific_sequence(sequence_definition, num_elements):
+    sequence = []
+    for n in range(num_elements):
+        n = n + 1 # sequences start at 1 instead of 0 and end at num_elements instead of num_elements - 1
+        val = float(eval(sequence_definition)) # be aware that eval can be a potentially dangerous function to use
+        sequence.append(val)
+    return sequence
