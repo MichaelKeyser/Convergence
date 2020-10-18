8f9155a39bbb6a3875850ed7f85eeabe2573525a
Michael Keyser <Michael@Michaels-MacBook-Air-4.local>
2020-10-17 18:57:45 -0600

    got files from trash and updated them

--- functions.py
+++ functions.py
@@ -1,13 +1,21 @@
+"""
+This script contains functions that generate sequences to be searched for the longest subsequence.
+Authors:
+"""
+
 import random as r
 
-
-# generates a list of n random floating point numbers within lower_bound and upper_bound
+"""
+Generates a list of n random floating point numbers within lower_bound and upper_bound
+"""
 def generate_random_sequence(lower, upper, n):
     sequence = []
     for i in range(n):
         sequence.append(r.uniform(lower,upper))
     return sequence
-
+"""
+Generates a sequence provided the definition and number of elements passed by the user
+"""
 def generate_specific_sequence(sequence_definition, num_elements):
     sequence = []
     for n in range(num_elements):
